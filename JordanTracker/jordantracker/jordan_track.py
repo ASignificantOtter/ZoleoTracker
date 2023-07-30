fcfe38848f247d207c6fdb6db96622d341882e0d
ASignificantOtter <angelobrett@gmail.com>
2023-07-30 12:47:58 -0400

    Hello world to prove I can python....

---
+++ JordanTracker/jordantracker/jordan_track.py
@@ -0,0 +1,18 @@
+# Hello world example to start this dang project....
+
+
+
+
+
+
+
+def helloworld(name):
+    print(f'Hi, {name}')
+    
+    return 0
+
+
+
+
+if __name__ == '__main__':
+    helloworld('Matt')
