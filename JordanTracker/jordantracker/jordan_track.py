1baaf5656706d84bc66a2fd3fac988fe84cded9c
ASignificantOtter <angelobrett@gmail.com>
2023-07-31 19:50:17 -0400

    Starting....

--- JordanTracker/jordantracker/jordan_track.py
+++ JordanTracker/jordantracker/jordan_track.py
@@ -1,8 +1,11 @@
-# Hello world example to start this dang project....
+# Email parser / CSV writer of Jordan's location
 
+import os
+import csv
+import email
 
-
-
+from datetime import datetime as dt
+from tkinter.filedialog import askdirectory
 
 
 
@@ -11,8 +14,21 @@ def helloworld(name):
     
     return 0
 
+def parse_email(timestamp, folder_path):
+    #grab emaiils in folder newer than latest timestamp in csv
+
+    return 0
+
+def stamp_time(none):
+    now = dt.now()
+    timestamp = now.strftime("%d/%m/%Y %H:%M:%S")
+
+    return timestamp
 
 
 
 if __name__ == '__main__':
-    helloworld('Matt')
+    #helloworld('Matt')
+
+    folder_path = os.path.normpath(askdirectory(title='Select Folder'))    
+    
