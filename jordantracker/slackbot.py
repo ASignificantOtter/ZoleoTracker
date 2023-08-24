9c33e81c7afc142a866892851d3a153c7d18cf39
ASignificantOtter <angelobrett@gmail.com>
2023-08-24 13:40:18 -0700

    last pr update I swear

--- jordantracker/slackbot.py
+++ jordantracker/slackbot.py
@@ -14,7 +14,7 @@ def file_path(filename: str) -> str:
     return file_path
 
 
-def import_csv() -> tuple[str, str, str]:
+def get_current_checkin() -> tuple[str, str, str]:
     csv_data = []
     csv_file_path = file_path('/location.csv')
 
@@ -32,7 +32,7 @@ def import_csv() -> tuple[str, str, str]:
     return date, gps, loc_link
 
 def should_post(previous_checkin, current_checkin) -> bool:
-    #logic to post or not
+
     if current_checkin > previous_checkin:
         return True
 
@@ -46,11 +46,11 @@ def get_previous_checkin() -> str:
 
 def post_location() -> None:
     
-    current_checkin, gps, loc_link = import_csv()
+    current_checkin, gps, loc_link = get_current_checkin()
     previous_checkin = get_previous_checkin()
 
     if should_post(previous_checkin, current_checkin):
         client = slack.WebClient(token=config.SLACK_TOKEN)
         client.chat_postMessage(channel='#jordan-tracker',text='I was last here: ' + gps + '\nat this time: ' + current_checkin + '\n' + loc_link)
-        with open([file_path('/previous_checkin.txt')], 'w', encoding='utf-8') as previous_checkin_text_fp:
+        with open(file_path('/previous_checkin.txt'), 'w', encoding='utf-8') as previous_checkin_text_fp:
          previous_checkin_text_fp.writelines(current_checkin)
