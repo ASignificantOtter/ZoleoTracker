b85e82e43a8a0775d574414ac05e7e86c2fb18bc
ASignificantOtter <angelobrett@gmail.com>
2023-08-23 17:43:51 -0700

    finished logic

--- jordantracker/slackbot.py
+++ jordantracker/slackbot.py
@@ -6,13 +6,19 @@ from jordantracker import config
 
 
 
-def file_path() -> str: 
+def csv_file_path() -> str:
     cwd = os.path.dirname(os.path.realpath(__file__))
     parent_directory = os.path.dirname(cwd)
     csv_file_path = os.path.join(parent_directory, parent_directory+"/location.csv")
 
     return csv_file_path
 
+def text_file_path() -> str:
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    parent_directory = os.path.dirname(cwd)
+    text_file_path = os.path.join(parent_directory, parent_directory+"/previous_checkin.txt")
+
+    return text_file_path
 
 def import_csv(csv_file_path) -> tuple[str, str, str]:
     csv_data = []
@@ -35,11 +41,26 @@ def decide_to_post(previous_checkin, current_checkin) -> bool:
     if current_checkin > previous_checkin:
         return True
 
+def get_previous_checkin(text_file) -> str:
+    file = open(text_file, 'r', encoding='utf-8')
+    previous_checkin = file.read()
+    file.close()
+
+    return previous_checkin
 
 def post_location() -> None:
 
-    csv_file = file_path()
-    date, gps, loc_link = import_csv(csv_file)
+    csv_file = csv_file_path()
+    text_file = text_file_path()
+    current_checkin, gps, loc_link = import_csv(csv_file)
+    previous_checkin = get_previous_checkin(text_file)
+    #previous_checkin = current_checkin
+
 
     client = slack.WebClient(token=config.SLACK_TOKEN)
-    client.chat_postMessage(channel='#jordan-tracker',text='I was last here: ' + gps + '\nat this time: ' + date + '\n' + loc_link)
+
+    if decide_to_post(previous_checkin, current_checkin) is True:
+        client.chat_postMessage(channel='#jordan-tracker',text='I was last here: ' + gps + '\nat this time: ' + current_checkin + '\n' + loc_link)
+        previous_checkin_text = open(text_file, 'w', encoding='utf-8')
+        previous_checkin_text.writelines(current_checkin)
+        previous_checkin_text.close()
