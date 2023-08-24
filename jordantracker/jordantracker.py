c366cf24fbb6d9ccfb51bcf8b4228a565117a797
ASignificantOtter <angelobrett@gmail.com>
2023-08-24 16:21:21 -0700

    Added parse email from server

--- jordantracker/jordantracker.py
+++ jordantracker/jordantracker.py
@@ -7,12 +7,90 @@ import pandas as pd
 import html2text
 import re
 from datetime import datetime
+from jordantracker import config
+
+import email
+import imaplib
+import base64
 
 from email import policy
 from email.parser import BytesParser
 from datetime import datetime as dt
 
-def parse_email(folder_path):
+def parse_email_server():
+
+    file_names = []
+    texts = []
+    locations = []
+    checkins = []
+    links = []
+
+    mail = imaplib.IMAP4_SSL(config.EMAIL_SERVER)
+    mail.login(config.EMAIL_ACCOUNT, config.EMAIL_PASSWORD)
+    mail.select('inbox')
+    status, data = mail.search(None, 'ALL')
+    mail_ids = []
+    for block in data:
+        mail_ids += block.split()
+
+    for i in mail_ids:
+        status, data = mail.fetch(i, '(RFC822)')
+        for response_part in data:
+            if isinstance(response_part, tuple):
+                message = email.message_from_bytes(response_part[1])
+                #mail_from = message['from']
+                mail_subject = message['subject']
+                if mail_subject == 'Check-in message from SlothPace':
+
+                    if message.is_multipart():
+                        mail_content = ''
+
+                        for part in message.get_payload():
+                            if part.get_content_type() == 'text/plain':
+                                mail_content += part.get_payload()
+                                mail_content = base64.b64decode(mail_content)
+                                mail_content_text = mail_content.decode(errors='ignore')
+                                text = html2text.html2text(mail_content_text)
+                        
+
+                    else:
+                        mail_content = message.get_payload()
+                        mail_content = base64.b64decode(mail_content)
+                        mail_content_text = mail_content.decode(errors='ignore')
+                        text = html2text.html2text(mail_content_text)
+                    
+        
+        try:
+            location = re.search(r"My location is (.+)", text).group(1)
+        except AttributeError:
+            location = re.search(r"My location is (.+)", text)
+        
+        try:
+            checkin = re.search(r"sent at: (.+) \(UTC\)", text).group(1)
+        except AttributeError:
+            checkin = re.search(r"sent at: (.+) \(UTC\)", text)
+
+        checkin = stamp_time(checkin)
+
+        try:
+            link = str(re.search(r"View on map \]\((.+)\)", text).group(1))
+        except AttributeError:
+            link = str(re.search(r"View on map \]\((.+)\)", text))
+
+        
+        file_names.append(mail_subject)
+        texts.append(text)
+        locations.append(location)
+        checkins.append(checkin)
+        links.append(link)
+    df_location = pd.DataFrame([file_names, checkins, locations, links]).T
+    df_location.columns = ['file', 'checkin', 'location', 'link']
+    
+    return df_location
+                        
+
+
+def parse_email_file(folder_path):
     eml_files = glob.glob(folder_path + '*.eml') # get all .eml files in a list
     file_names = []
     texts = []
@@ -67,22 +145,21 @@ def fpath():
 
 
 def stamp_time(checkin):
-    #now = dt.now()
-    #curtime = now.strftime("%d %b %Y %H:%M:%S")
-    
-    #examplestr = '30 Jul 2023 17:26:53'
+
     datetime_format = '%d %b %Y %H:%M:%S'
     timestamp = datetime.strptime(checkin, datetime_format)
 
     return timestamp
 
 def tracker():
-    folder_path = fpath()
-    df_location = parse_email(folder_path) 
+
+    #df_location_old = parse_email_file(fpath())
+    df_location = parse_email_server()
     df_location.sort_values(by='checkin', inplace=True)
 
-    #appens csv
     df_location.to_csv('location.csv', sep='\t', index=False,header=True)
+    #df_location_old.to_csv('location_old.csv', sep='\t', index=False,header=True)
+
 
 
 if __name__ == '__main__':
