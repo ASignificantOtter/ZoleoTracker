51c1e146f7f6dd5b797ed5e8d6dccfc8a11b8d88
ASignificantOtter <angelobrett@gmail.com>
2023-08-26 09:59:32 -0700

    undo main.py mistakes

--- jordantracker/__main__.py
+++ jordantracker/__main__.py
@@ -2,6 +2,5 @@ from jordantracker import jordantracker
 from jordantracker import slackbot
 
 
-def run():
-    jordantracker.tracker()
-    slackbot.post_location()
+jordantracker.tracker()
+slackbot.post_location()
