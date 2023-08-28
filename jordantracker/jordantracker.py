1a47ae09e6c5c28ad31c7fd8375ff6da8afbded5
ASignificantOtter <140848822+ASignificantOtter@users.noreply.github.com>
2023-08-27 20:26:37 -0700

    Merge Wip-email-server into main(#11)
    * Added parse email from server
    
    * cleanup of working code
    
    * PR cleanup, small refactor
    
    * quick updates
    
    * PR updates
    
    * undo main.py mistakes
    
    * Last PR fixes

--- jordantracker/jordantracker.py
+++ jordantracker/jordantracker.py
@@ -1,32 +1,66 @@
 # Email parser / CSV writer of Jordan's location
 
-import os
-#import csv
-import glob
-import pandas as pd
-import html2text
 import re
 from datetime import datetime
+import base64
+import email
+import imaplib
+import html2text
+import pandas as pd
+from jordantracker import config
 
-from email import policy
-from email.parser import BytesParser
-from datetime import datetime as dt
+PLAIN_TEXT = 'text/plain'
 
-def parse_email(folder_path):
-    eml_files = glob.glob(folder_path + '*.eml') # get all .eml files in a list
+#returns a IMAP4_SSL class object containing the entire email folder from server
+def get_inbox():
+    mail = imaplib.IMAP4_SSL(config.EMAIL_SERVER)
+    mail.login(config.EMAIL_ACCOUNT, config.EMAIL_PASSWORD)
+    mail.select(config.EMAIL_FOLDER)
+
+    return mail
+
+# Connects to email account from config file, searches through specified email folder, 
+# looks for check-in emails by subject, parses out the checkin time, location, and google maps link. 
+# Returns a dataframe of [email_subject, checkin time, checkin location, location google maps link]
+def parse_email_server() -> pd.DataFrame:
+ 
     file_names = []
     texts = []
     locations = []
     checkins = []
     links = []
 
-    for file in eml_files:
-        with open(file, 'rb') as fp:
-            name = fp.name  # Get file name
-            msg = BytesParser(policy=policy.default).parse(fp)
-        text = msg.get_body().get_content()
-        text = html2text.html2text(text)
-       
+    mail = get_inbox()
+    mail_status, inbox = mail.search(None, 'ALL')
+    mail_ids = []
+    # splits out individual email messages from one blob (inbox)
+    # iterates through each individual email id, and splits from the full inbox block
+    for mail_block in inbox:
+        mail_ids += mail_block.split()
+    # iterates through email messages using email id as an iterator. 
+    for i in mail_ids:
+        status, raw_email = mail.fetch(i, config.MESSAGE_FORMAT)
+        for response_part in raw_email:
+            if isinstance(response_part, tuple):
+                message = email.message_from_bytes(response_part[1])
+
+                if message['subject'] == config.CHECKIN_EMAIL_SUBJECT:
+
+                    if message.is_multipart():
+                        mail_content = ''
+
+                        for part in message.get_payload():
+                            if part.get_content_type() == PLAIN_TEXT:
+                                mail_content += part.get_payload()
+                                mail_content = base64.b64decode(mail_content)
+                                
+                    else:
+                        mail_content = base64.b64decode(message.get_payload())
+                
+                mail_content_text = mail_content.decode(errors='ignore')
+                text = html2text.html2text(mail_content_text) 
+                    
+        
         try:
             location = re.search(r"My location is (.+)", text).group(1)
         except AttributeError:
@@ -44,46 +78,29 @@ def parse_email(folder_path):
         except AttributeError:
             link = str(re.search(r"View on map \]\((.+)\)", text))
 
-        
-        
-        file_names.append(name)
+        file_names.append(message['subject'])
         texts.append(text)
         locations.append(location)
         checkins.append(checkin)
         links.append(link)
-        fp.close()
-
-    df_location = pd.DataFrame([file_names, checkins, locations, links]).T
-    df_location.columns = ['file', 'checkin', 'location', 'link']
+    df_all_checkins = pd.DataFrame([file_names, checkins, locations, links]).T
+    df_all_checkins.columns = ['file', 'checkin', 'location', 'link']
     
-    return df_location
+    return df_all_checkins
+               
+def stamp_time(checkin) -> datetime:
 
-def fpath():
-    current = os.path.dirname(os.path.realpath(__file__))
-    parent = os.path.dirname(current)
-    folder_path = os.path.join(parent, 'emails/')
-
-    return folder_path
-
-
-def stamp_time(checkin):
-    #now = dt.now()
-    #curtime = now.strftime("%d %b %Y %H:%M:%S")
-    
-    #examplestr = '30 Jul 2023 17:26:53'
     datetime_format = '%d %b %Y %H:%M:%S'
     timestamp = datetime.strptime(checkin, datetime_format)
 
     return timestamp
 
-def tracker():
-    folder_path = fpath()
-    df_location = parse_email(folder_path) 
-    df_location.sort_values(by='checkin', inplace=True)
+def tracker() -> None:
 
-    #appens csv
-    df_location.to_csv('location.csv', sep='\t', index=False,header=True)
-
+    df_all_checkins = parse_email_server()
+    #sorts from oldest checkin to latest checkin
+    df_all_checkins.sort_values(by='checkin', inplace=True) 
+    df_all_checkins.to_csv('location.csv', sep='\t', index=False,header=True)
 
 if __name__ == '__main__':
     tracker()
